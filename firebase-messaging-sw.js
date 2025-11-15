4dd6f7e442ec1255314a849012acd2cf3a24ac75
kanishklahoti <kanishklahoti9@gmail.com>
2025-11-15 23:25:15 +0530

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging.js');
+importScripts("https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/10.8.0/firebase-messaging.js");
 
 firebase.initializeApp({
   apiKey: "AIzaSyDdCxvQw_oIgf-Jfmrw3oLhx236Ydaxs6w",
@@ -7,13 +7,12 @@ firebase.initializeApp({
   projectId: "ecosmart-b4613",
   storageBucket: "ecosmart-b4613.firebasestorage.app",
   messagingSenderId: "1032334615608",
-  appId: "1:1032334615608:web:25ad6d59ca342ad637b2cd",
-  measurementId: "G-YL1QP7C4G5"
+  appId: "1:1032334615608:web:25ad6d59ca342ad637b2cd"
 });
 
 const messaging = firebase.messaging();
 
-// Background notification handler
+// Background Push Handler
 messaging.onBackgroundMessage((payload) => {
   self.registration.showNotification(payload.notification.title, {
     body: payload.notification.body,
